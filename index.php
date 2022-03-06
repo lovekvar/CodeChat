326aafd76b5cfa89d95dbf2637dacd7d805fbef8
lovekvar <85835517+lovekvar@users.noreply.github.com>
2022-03-06 12:41:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,30 @@
 
 <body>
     hello india
-    <?php include 'partials/_navbar.php'; ?>
+    
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/Best-Programming-Languages-to-Start-Learning-Today.jpg" style= "height: 60vh" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/machine-learning-banner-web-icon-260nw-1110900704.jpg" style= "height: 60vh" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://www.monocubed.com/wp-content/uploads/2021/03/7-Phases-of-web-development-life-cycle.jpg" style= "height: 60vh" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 </body>
 
 </html>
