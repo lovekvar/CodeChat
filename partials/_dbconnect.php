4494deb24969ecdbb32e5fecbccf85390d4b133d
lovekvar <85835517+lovekvar@users.noreply.github.com>
2022-03-07 00:03:18 +0530

    Update _dbconnect.php

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -6,11 +6,11 @@
     $database = "codechat";
 
     // Create a connection
-    $conn = mysqli_connect($servername, $username, $password, $database);
+//     $conn = mysqli_connect($servername, $username, $password, $database);
     // Die if connection was not successful
-    if (!$conn){
-        die("Sorry we failed to connect: ". mysqli_connect_error());
-    }
+//     if (!$conn){
+//         die("Sorry we failed to connect: ". mysqli_connect_error());
+//     }
     // else{
     //     echo "Connection was successful<br>";
     // }
