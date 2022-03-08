9f100ae64bd992299c9fab40ebf47cb619abb6e4
lovekvar <85835517+lovekvar@users.noreply.github.com>
2022-03-08 16:22:47 +0530

    Update _dbconnect.php

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,16 +1,16 @@
 <?php
     // Connecting to the Database
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "codechat";
+    $servername = "sql6.freemysqlhosting.net";
+    $username = "sql6477370";
+    $password = "I5MVayTvbF";
+    $database = "I5MVayTvbF";
 
     // Create a connection
-//     $conn = mysqli_connect($servername, $username, $password, $database);
+    $conn = mysqli_connect($servername, $username, $password, $database);
     // Die if connection was not successful
-//     if (!$conn){
-//         die("Sorry we failed to connect: ". mysqli_connect_error());
-//     }
+    if (!$conn){
+        die("Sorry we failed to connect: ". mysqli_connect_error());
+    }
     // else{
     //     echo "Connection was successful<br>";
     // }
